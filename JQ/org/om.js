c9f0a1cfafa22c5a592da32e80ab9df43a7e7f5f
15302160404 <15767314719@189.cn>
2018-05-24 10:09:05 +0800

    a

--- JQ/org/om.js
+++ JQ/org/om.js
@@ -1,15 +1,38 @@
+var bool = false;
 $(document).ready(function () {
-    $('#vegOn').click(function () {
-        //删除fish
-        $save = $('.fish').parent().parent().detach();
-        //替换汉堡包为大蘑菇
-        $('.hamburger').replaceWith('<li class="mushroom">mushroom</li>');
-        //把meat 替换成tufo
-        $('.meat').replaceWith('<li class="tofu">tofu</li>');
-        $('.menu_list').parent().addClass('veg_leaf');
-    });
-    $('#restoreMe').click(function () {
-        //还原
-        history.go(0);
-    });
+    if(bool == false){
+        $('#vegOn').click(function () {
+            //删除fish
+            $f = $('.fish').parent().parent().detach();
+            //替换汉堡包为大蘑菇
+            $('.hamburger').replaceWith('<li class="mushroom">mushroom</li>');
+            //把meat 替换成tufo
+            $('.meat').after('<li class="tofu">tofu</li>');
+            $m = $('.meat').detach();
+            $('.tofu').parent().parent().addClass('veg_leaf');
+            $('.mushroom').parent().parent().addClass('veg_leaf');
+            // $(this).unbind('click');
+            bool = true;
+            console.log(bool);
+        });
+    }
+        $('#restoreMe').click(function () {
+            if(bool == true){
+            //还原fish
+            $('.menu_entrees li').first().before($f);
+            //大蘑菇还原为汉堡包
+            $('.mushroom').replaceWith('<li class="hamburger">hamburger</li>');
+            //tofu还原成meat
+            $('.tofu').each(function(i){
+                $(this).after($m[i]);
+            });
+            $('.tofu').remove();
+            $('.meat').parent().parent().removeClass('veg_leaf');
+            $('.hamburger').parent().parent().removeClass('veg_leaf');
+            // $(this).unbind('click');
+            bool = false;
+            console.log(bool);
+        }
+        });
+    
 });
