dce768e20e487540f50312406b1e2398fe6e5e55
15302160404 <15767314719@189.cn>
2018-06-07 10:00:05 +0800

    2018-6-7

--- JQ/org/ds.js
+++ JQ/org/ds.js
@@ -1,58 +1,30 @@
 $(document).ready(function() {
-	var headclix = 0,eyeclix = 0,noseclix = 0,mouthclix = 0;
-	lightning_one(4000);
-	lightning_two(5000);
-	lightning_three(7000);
-	$('#head').click(function() {
-		if(headclix < 9){
-			$(this).animate({left: '-=367px',},500);
-			headclix += 1;
-		}
-		else{
-			$(this).animate({left: '0px',},500);
-			headclix = 0;
-		}
-	});
-	$('#eyes').click(function() {
-		if(eyeclix < 9){
-			$(this).animate({left: '-=367px',},500);
-			eyeclix += 1;
-		}
-		else{
-			$(this).animate({left: '0px',},500);
-			eyeclix = 0;
-		}
-	});
-	$('#nose').click(function() {
-		if(noseclix < 9){
-			$(this).animate({left: '-=367px',},500);
-			noseclix += 1;
-		}
-		else{
-			$(this).animate({left: '0px',},500);
-			noseclix = 0;
-		}
-	});
-	$('#mouth').click(function() {
-		if(mouthclix < 9){
-			$(this).animate({left: '-=367px',},500);
-			mouthclix += 1;
-		}
-		else{
-			$(this).animate({left: '0px',},500);
-			mouthclix = 0;
-		}
-	});
-	function lightning_one(t){
-		$('#container #lightning1').fadeIn(250).fadeOut(250);
-		setTimeout('lightning_one()',t);
-	};
-	function lightning_two(t){
-		$('#container #lightning2').fadeIn('fast').fadeOut('fast');
-		setTimeout('lightning_two()',t);
-	};
-	function lightning_three(t){
-		$('#container #lightning3').fadeIn('fast').fadeOut('fast');
-		setTimeout('lightning_three()',t);
-	};
+	var num =0;
+	goLightning();
+	window.onblur= stopLightning;
+	window.onfocus = goLightning;
+	face('#head',num);
+	face('#eyes',num);
+	face('#nose',num);
+	face('#mouth',num);
+	function goLightning(){
+		setInterval('$("#lightning1").fadeIn(250).fadeOut(250)',2000);
+		setInterval('$("#lightning2").fadeIn("fast").fadeOut("fast")',3000);
+		setInterval('$("#lightning3").fadeIn("fast").fadeOut("fast")',4000);
+	}
+	function stopLightning(){
+		clearInterval(goLightning());
+	}
+	function face(id,num){
+		$(id).click(function(){
+			if(num < 9){
+				$(this).animate({left: '-=367px',},500);
+				num += 1;
+			}
+			else{
+				$(this).animate({left: '0px',},500);
+				num = 0;
+			}
+		});
+	}
 });
