08e2bee422641a79cde80018071bc553511dba44
15302160404 <15767314719@189.cn>
2018-06-12 09:58:01 +0800

    finish

--- JQ/org/ds.js
+++ JQ/org/ds.js
@@ -2,6 +2,8 @@ var a1,a2,a3;
 $(document).ready(function() {
 	var num =0;
 	goLightning();
+	ra();
+	re();
 	window.onblur= stopLightning;
 	window.onfocus = goLightning;
 	face('#head',num);
@@ -29,5 +31,29 @@ $(document).ready(function() {
 				num = 0;
 			}
 		});
+	}
+	function ra(){
+		$("#ra").click(function(){
+			var b1 = randomNum()*367;
+			var b2 = randomNum()*367;
+			var b3 = randomNum()*367;
+			var b4 = randomNum()*367;
+			$("#head").animate({left: "-"+b1+'px',},500);
+			$("#eyes").animate({left: "-"+b2+'px',},500);
+			$("#nose").animate({left: "-"+b3+'px',},500);
+			$("#mouth").animate({left: "-"+b4+'px',},500);
+		});
+	}
+	function re(){
+		$("#re").click(function(){
+			var c = 0;
+			$("#head").animate({left: c+'px',},500);
+			$("#eyes").animate({left: c+'px',},500);
+			$("#nose").animate({left: c+'px',},500);
+			$("#mouth").animate({left: c+'px',},500);
+		});
+	}
+	function randomNum(){
+		return parseInt(Math.random()*9);
 	}
 });
